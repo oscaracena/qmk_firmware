550322b017ee195c01de31489d1f6b825a9cc4b4
Oscar Aceña <oscaracena@gmail.com>
2024-09-14 00:30:56 +0200

    keepan: LEDs, buttons, DIPs and encoders working on breadboard

--- keyboards/keepan/keymaps/default/keymap.c
+++ keyboards/keepan/keymaps/default/keymap.c
@@ -1,21 +1,19 @@
-// Copyright 2023 QMK
+// Copyright 2024 Oscar Acena
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
-     */
     [0] = LAYOUT(
-        KC_P7
+        KC_W, KC_S
     )
+};
+
+const uint16_t PROGMEM dip_switch_map[NUM_DIP_SWITCHES][NUM_DIP_STATES] = {
+    DIP_SWITCH_OFF_ON(KC_T, KC_G),
+    DIP_SWITCH_OFF_ON(KC_R, KC_F)
+};
+
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(KC_U, KC_J)},
 };
---
+++ keyboards/keepan/rules.mk
@@ -0,0 +1,4 @@
+DIP_SWITCH_MAP_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+DEBOUNCE_TYPE = sym_defer_g
